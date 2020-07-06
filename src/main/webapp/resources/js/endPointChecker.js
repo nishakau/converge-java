c8109ed349347887696008d120222313480692a7
nishakau <nishant.k.kaushik@oracle.com>
2020-07-06 19:47:05 +0530

    Added spatial rest api
    Added spatial rest api
    removed harcded url from the page
    Seprated the js and html file

--- src/main/webapp/resources/js/endPointChecker.js
+++ src/main/webapp/resources/js/endPointChecker.js
@@ -1,5 +1,4 @@
-
-    var globalurl = "http://localhost:7001/";
+var globalurl = "http://localhost:7001/";
     //var globalProductId = null;
     var xmlIdsDropdown = new Array();
     $(document).ready(function(){
@@ -11,7 +10,7 @@
   	  $("#selectXmlDocumentId").empty();
   	  $.ajax({
             method: "GET",
-            url: "http://localhost:7001/xml/ids",
+            url: "/xml/ids",
             success: function (data) {
           	  console.log("ids");
           	  
@@ -35,16 +34,34 @@
           });
     }
     
-    
+    function hideAll(){
+          var containers = ["#xmlcontainer","#jsoncontainer","#spatialcontainer","#crosscontainer"];
+          for(var i=0;i<containers.length;i++){
+              $(containers[i]).hide();
+          }
+          
+    }
     function callView() {
       var item = document.getElementById("selectID");
+    
+      hideAll();
       if (item.value == 1) {
-        $("#xmlcontainer").hide();
+        //$("#xmlcontainer").hide();
         $("#jsoncontainer").show();
       }
       if (item.value == 2) {
-        $("#jsoncontainer").hide();
+       // $("#jsoncontainer").hide();
         $("#xmlcontainer").show();
+      }
+     if (item.value == 3) {
+       // $("#jsoncontainer").hide();
+       
+        $("#spatialcontainer").show();
+        getAllCity();
+      }
+       if (item.value == 4) {
+       // $("#jsoncontainer").hide();
+        $("#crosscontainer").show();
       }
     }
 
@@ -70,7 +87,7 @@
   	 }
       $.ajax({
         method: "GET",
-        url: "http://localhost:7001/json/product/"+id.value,
+        url: "/json/product/"+id.value,
         dataType:'json',
         success: function (data) {
       	 let item = document.querySelector("#jsonBodyContainer");
@@ -109,7 +126,7 @@
   	  //console.log(obj);
   	  $.ajax({
             method: "POST",
-            url: "http://localhost:7001/json/product/update",
+            url: "/json/product/update",
             dataType:'text',
             contentType: 'application/json',
             data:JSON.stringify(obj),
@@ -136,7 +153,7 @@
   	  console.log(obj);
   	  $.ajax({
             method: "POST",
-            url: "http://localhost:7001/json/product/delete",
+            url: "/json/product/delete",
             contentType: 'application/json',
             dataType:'text',
             data:JSON.stringify(obj),
@@ -171,7 +188,7 @@
   	  console.log(obj);
   	  $.ajax({
             method: "POST",
-            url: "http://localhost:7001/json/product/insert",
+            url: "/json/product/insert",
             contentType: 'application/json',
             dataType:'text',
             data:JSON.stringify(obj),
@@ -191,7 +208,7 @@
   	  var item =document.getElementById("selectXmlDocumentId");
   	  $.ajax({
             method: "GET",
-            url: "http://localhost:7001/xml/read/"+item.value,
+            url: "/xml/read/"+item.value,
             dataType:'xml',
             success: function (data) {
           	 console.log(data);
@@ -215,7 +232,7 @@
     	var data = {query:query,id:id,value:value};
     	$.ajax({
       		method:"POST",
-      		url:"http://localhost:7001/xml/update",
+      		url:"/xml/update",
       		contentType: "application/json",
       	    dataType: "text",
       		data:JSON.stringify(data),
@@ -237,7 +254,7 @@
     	let data = {id:item};
     	$.ajax({
     		method:"POST",
-    		url:"http://localhost:7001/xml/delete",
+    		url:"/xml/delete",
     		contentType: "application/json",
       	    dataType: "text",
       	  	data:JSON.stringify(data),
@@ -301,7 +318,7 @@
   	  }
   	 $.ajax({
   		method:"POST",
-  		url:"http://localhost:7001/xml/insert",
+  		url:"/xml/insert",
   		contentType: "text/xml",
   	    dataType: "text",
   		data:data,
@@ -343,3 +360,196 @@
 		}
 		return false;
 	}
+        
+
+      //Spatial functions
+      
+       var dataObject = [{id:'1',name:'test',long:'-74.006058',lat:'38.91254'},{id:'2',name:'test_2',lat:'-75.006058',long:'39.91254'}];
+      function getAllCity(){
+       var selectList  = document.getElementById("selectSpatialCityId");
+  	  $("#selectSpatialCityId").empty();
+         $.ajax({
+        method: "GET",
+        url: "/spatial/allCities",
+        dataType:'json',
+        success: function (data) {
+            dataObject =data;
+            
+            for(let i=0;i<data.length;i++){
+                var option = document.createElement("option");
+                option.value = data[i].id;
+                option.text = data[i].name;
+                selectList.appendChild(option);
+                
+            }
+             
+            showMap();
+        },
+        error: function (xhr,err) {
+      	  console.log(err);
+          return [];
+            
+        }
+      });   
+      }
+      
+     
+      function unpinCity(){
+      var item =document.getElementById("selectSpatialCityId");
+      var obj = {id:item.value};
+      $.ajax({
+            method: "POST",
+            url: "/spatial/delete",
+            contentType: 'application/json',
+            dataType:'text',
+            data:JSON.stringify(obj),
+            success: function (data) {
+                alert(data);
+          	 getAllCity();
+            },
+            error: function (err) {
+              console.log("error occured from server");
+            }
+          });
+      
+      }
+      
+      function insertNewCity(){
+          var cityElm = document.getElementById("spatialCity").value;
+          var cityLong = document.getElementById("spatialLong").value;
+          var cityLat = document.getElementById("spatialLat").value;
+        
+          if(cityElm.length<1 || cityLong.length <1 || cityLat.length<1){
+            alert("missing info");
+            return;
+          }
+          
+          var obj = {name:cityElm,long:cityLong,lat:cityLat};
+          console.log("From browser bfore insert");
+          console.log(obj);
+          $.ajax({
+            method: "POST",
+            url: "/spatial/insert",
+            contentType: 'application/json',
+            dataType:'text',
+            data:JSON.stringify(obj),
+            success: function (data) {
+                alert("Inserted");
+                getAllCity();
+            },
+            error: function (err) {
+              console.log("error occured from server");
+            }
+          });
+          
+          
+      }
+      
+      function showMap()
+      {      
+          var baseURL  = "http://maps.oracle.com/mapviewer";
+         
+          var getData = dataObject;
+          console.log(getData)
+          if(getData.length>0){
+           var mapCenterLon = getData[0].long;
+          var mapCenterLat = getData[0].lat;
+           var mapZoom      =  3;  
+          var mpoint = new OM.geometry.Point(mapCenterLon,mapCenterLat,8307);
+          }
+         
+         
+          
+          var map = new OM.Map(
+          document.getElementById('map'),
+          {
+              mapviewerURL: baseURL
+          }) ;       
+          var tileLayer = new OM.layer.TileLayer( "baseMap", 
+          {
+              dataSource:"elocation_mercator", 
+              tileLayer:"world_map", 
+              tileServerURL:baseURL+"/mcserver"
+          });
+          
+          map.addLayer(tileLayer) ;
+
+          layer = new OM.layer.MarkerLayer("markerlayer4");
+
+          // set all properties after a map marker is added, srid is default (8307)
+        var insertMapMarker1 = function(id, cx, cy,  label, draggable) {
+                var mm = new OM.MapMarker();
+                layer.addMapMarker(mm);   // add a map marker into marker layer
+                mm.setPosition(cx, cy);
+               // mm.setMarkerText(text);  // it will also set the marker text. 
+                mm.setLabel(label);  // it will also set the marker text. 
+                mm.setID(id);
+               
+                var renderStyle = new OM.style.Marker({
+                      width: 50,
+                      height: 50,
+                      src: "/resources/images/pin.png",
+                      xOffset: 0,
+                      yOffset: 0
+                    });
+                var textStyle = new OM.style.Text({
+                    styleName:"txt",
+                    fill:"#00a000",
+                    fontStyle: OM.Text.FONTSTYLE_NORMAL,
+                    fontFamily:"Arial",
+                    fontSize:0
+                });
+                var labelStyle = new OM.style.Text({
+                    //styleName: '',
+                    fill: "#00a000",
+                    fontStyle: OM.style.Text.FONTSTYLE_ITALIC,
+                    fontSize: 0,
+                    sticky: false
+                });
+                mm.setRenderingStyle(renderStyle);                
+                mm.setMarkerTextStyle(textStyle);                
+                mm.setLabelingStyle(labelStyle);  
+        };
+        // an obj with all properties to instantiate a map marker
+        var insertMapMarker4 = function(id, cx, cy, srid, text, label) {
+                var myobj = {id: id,
+                             markerText: text,
+                             label: label,
+                             dragStart: dragStart,
+                             dragEnd: dragEnd,
+                             dragging: dragging,
+                             position: {'x': cx, 'y': cy,'srid': srid}};
+                var mm = new OM.MapMarker(myobj);
+                layer.addMapMarker(mm);   // add a map marker into marker layer
+        };
+        
+        for (var i = 0; i < getData.length; i++) {
+        insertMapMarker1(getData[i].id, getData[i].long, getData[i].lat,getData[i].name, false);  // id, cx, cy, srid, label, draggable
+        //insertMapMarker4(getData[i].STORE_ID, getData[i].CENTER_LONG, getData[i].CENTER_LAT, 8307, 'M'); 
+      }
+        var defaultLabelSty = new OM.style.Text({
+            //styleName: 'defaultLabelSty', 
+            fill: "#ffffff",
+            fontStyle: OM.style.Text.FONTSTYLE_ITALIC,
+            fontSize: 12,
+            sticky: false
+        });
+        layer.setLabelingStyle(defaultLabelSty);  // you need set a map marker's labeling style from a map marer instance
+        map.addLayer(layer);
+        layer.setToolTipCustomizer(featureTip);
+        layer.setLabelsVisible(true);
+
+
+          navigationPanelBar=new OM.control.NavigationPanelBar();
+          map.addMapDecoration(navigationPanelBar);
+          map.setMapCenter(mpoint);
+          
+          map.setMapZoomLevel(mapZoom) ;
+          map.init() ;
+          
+        }   
+        
+    function featureTip(f)
+        {
+        return "STORE ID: "+f.id+"\nSTORE NAME: "+f.label
+    }
